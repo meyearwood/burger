e7881678f1f56d0381807ce04b42effd531ce6bc
meyearwood <margoyearwood@gmail.com>
2017-12-19 19:44:32 -0500

    update to db

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+
+insert into burgers_tbl(burger_name) values ('Hamburger');
+insert into burgers_tbl(burger_name) values ('Cheeseburger');
+insert into burgers_tbl(burger_name) values ('Avocado Burger');
+insert into burgers_tbl(burger_name) values ('Turkey Burger');
+insert into burgers_tbl(burger_name) values ('Veggie Burger');
+insert into burgers_tbl(burger_name) values ('Vegan Burger');
+insert into burgers_tbl(burger_name) values ('Cowboy Burger');
