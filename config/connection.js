436aefe28b5c240d2601109b7dd2e012f7cce85b
meyearwood <margoyearwood@gmail.com>
2018-01-07 23:08:48 -0500

    config update

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
 var mysql = require('mysql');
+
 var jawsDBConn = mysql.createConnection({
+    port: 3306,
     host: 'ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user: 'hfc4u1l5ixznz12p',
     password: 'ruzvw6fvzgmnof7k',
@@ -16,13 +18,16 @@ var devConn = mysql.createConnection({
 var connection;
 
 if (process.env.NODE_ENV === 'production') {
+    console.log('PR!');
     connection = jawsDBConn;
 } else {
     connection = devConn;
 };
 
 connection.connect(function(err) {
+    console.log('connecting...');
     if (err) {
+        console.log('error connecting: ' + err.stack);
         console.error('error connecting: ' + err.stack);
         return;
     }
